ecad591014f55eef832c3abcd90e35e0446463d6
banana1019 <jyho930@gmail.com>
2022-05-06 00:03:29 +0900

    수정 버튼에 클릭 이벤트 바인딩

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,22 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  // TODO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
+  // - [x] 모달창에서 신규메뉴명을 입력받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    // 수정 버튼을 눌렀을 때
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   // form 태그가 자동으로 전송되는 걸 막아준다.
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -30,7 +46,7 @@ function App() {
             <button
               type="button"
               class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
+            > 
               수정
             </button>
             <button
